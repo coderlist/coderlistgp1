9fa045c727e3c5e9773262ea26f914703acb55d5
jidemobell <jidd32@gmail.com>
2018-05-09 18:40:59 +0100

    edited table & field names

--- coderlistGp1.sql
+++ coderlistGp1.sql
@@ -1,35 +1,35 @@
-CREATE TABLE "User" (
+CREATE TABLE "Pages" (
+  "page_published" timestamp,
+  "page_id" int,
+  "url" text,
+  "created_by" int,
+  "page_created" timestamp,
+  PRIMARY KEY ("page_id")
+);
+
+CREATE INDEX "FK" ON  "Pages" ("created_by");
+
+CREATE INDEX "Key" ON  "Pages" ("page_created");
+
+CREATE TABLE "Users" (
   "username" varchar(50),
-  "Email" varchar(50),
+  "email" varchar(50),
   "password" varchar(50),
-  "LastSuccesfulLogin" timestamp,
-  "LastFailedLogin" timestamp,
-  "FailedLoginAttemps" int,
+  "last_succesful_login" datetime,
+  "last_failed_login" timestamp,
+  "failed_login_attemps" int,
   "firstName" varchar(50),
   "lastName" varchar(50),
-  "userId" int,
-  PRIMARY KEY ("userId")
+  "user_id" int,
+  PRIMARY KEY ("user_id")
 );
 
-CREATE TABLE "Page" (
-  "pagePublished" timestamp,
-  "pageId" int,
-  "url" text,
-  "userId" int,
-  "pageCreated" timestamp,
-  PRIMARY KEY ("pageId")
+CREATE TABLE "Content" (
+  "content_Id" int,
+  "html_body" text,
+  "page_id" int,
+  PRIMARY KEY ("content_Id")
 );
 
-CREATE INDEX "FK" ON  "Page" ("userId");
-
-CREATE INDEX "Key" ON  "Page" ("pageCreated");
-
-CREATE TABLE "content" (
-  "contentId" int,
-  "htmlBody" text,
-  "pageId" int,
-  PRIMARY KEY ("contentId")
-);
-
-CREATE INDEX "FK" ON  "content" ("pageId");
+CREATE INDEX "FK" ON  "Content" ("page_id");
 
