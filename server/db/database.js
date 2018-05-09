715f50fa8c3d201600fc01e06c225c5770c219c9
jidemobell <jidd32@gmail.com>
2018-05-09 17:43:21 +0100

    made changes to database.js

--- server/db/database.js
+++ server/db/database.js
@@ -3,8 +3,8 @@ const env = process.env.NODE_ENV === 'production';
 
 const config = {
     "development":{
-        user: 'phemite', //'PGUSER',      
-        database: 'Test',//'PGDATABASE', 
+        user: 'PGUSER',      
+        database: 'PGDATABASE', 
         password: '',      
         host: 'localhost',     
         port: 5432,            
@@ -18,26 +18,6 @@ const config = {
 }
 
 const pool =   env ? new Pool(config.production): new Pool(config.development) ;
-
-// pool.on('error', (err, client) => {
-//     console.error('Unexpected error on idle client', err)
-//     process.exit(-1)
-//   })
-
-// const listQuery = `SELECT * FROM "User"`;
-// pool.connect()
-//     .then(client => {
-//         return client.query(listQuery)
-//                .then(res => {
-//                    client.release();
-//                    console.log(JSON.stringify(res.rows[0],undefined,2));
-//                })
-//     })
-//     .catch(e => {
-//        // client.release();
-//         console.log(e.stack);
-//     })
-
 
 
 module.exports = {
