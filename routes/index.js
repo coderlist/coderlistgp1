c65f69cc7c2ca4acf04feae7c9a303c6ea64db99
jidemobell <jidd32@gmail.com>
2018-05-12 08:51:35 +0100

    added test-script

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const {getAllUsers, createUser} = require('./../server/controllers/users');
 
 
-/* GET users listing. */
+
 routes.get('/users', getAllUsers);
 
 routes.post('/users', createUser);
