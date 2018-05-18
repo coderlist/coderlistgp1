adf8cec6f8c390895f23630bcb4e0fa0bf632bd2
ksigston <ksigston@blueyonder.co.uk>
2018-05-18 18:05:43 +0100

    add link expiry times

--- helperFunctions/verification/MailSender.js
+++ helperFunctions/verification/MailSender.js
@@ -11,7 +11,7 @@ class MailSender {
       from: process.env.EMAIL_NODEMAILER_USERNAME, // sender address
       to: userDetails.email, 
       subject: 'Password reset link for Ginny Bradley Website', 
-      text: 'You have requested a password reset. Please click the link and reset your password. This link will expire soon',
+      text: 'You have requested a password reset. Please click the link and reset your password. This link will expire in 60 minutes',
       html: `<a href="${config.url}?email=${userDetails.email}&token=${userDetails.verificationToken}>Click to be taken to the verification page</a><p> or copy and paste this link or browser. ${config.url}?email=${userDetails.email}&token=${userDetails.verificationToken}`
     }
     const transporter = nodemailer.createTransport(transportEmailConfig);
@@ -65,7 +65,7 @@ class MailSender {
       from: process.env.EMAIL_NODEMAILER_USERNAME,
       to: userDetails.email, 
       subject: 'Verification for Ginny Bradley Website', 
-      text: 'Here is a link to verify your email. You will asked to enter a password. The password has a minimum requirement of 8 characters./If this sign-up was not you then please ignore as the verification link will expire soon. Please feel free to email any concerns you may have. please click the below link to register your email.',
+      text: 'Here is a link to verify your email which will expire in one week. You will asked to enter a password. The password has a minimum requirement of 8 characters.If this sign-up was not you then please ignore as the verification link will expire soon. Please feel free to email any concerns you may have. please click the below link to register your email.',
       html: `<a href="${config.url}?email=${userDetails.email}&token=${userDetails.verificationToken}>Click to be taken to the verification page</a><p> or copy and paste this link or browser. ${config.url}?email=${userDetails.email}&token=${userDetails.verificationToken} </p>`
     }
     const transporter = nodemailer.createTransport(transportEmailConfig);
