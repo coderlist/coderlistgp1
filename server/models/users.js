b68b5e4eeee5076342f32851e925503eb3b59e93
jidemobell <jidd32@gmail.com>
2018-06-04 07:54:36 +0100

    update verifyUser and POST /enter-password

--- server/models/users.js
+++ server/models/users.js
@@ -21,18 +21,21 @@ module.exports = {
    * @param {Object} user 
    * gets token from route in a temporary user object
    * and compare with token on db where email === email 
-   * on success change activated to true
+   * on success, changes activated to true.
+   * It return a Promise
    */
   verifyUser(user){
-     findByUsername('users',user.email).then(dbUser => {
-       if(user.temporary_token === dbUser.temporary_token){
+     return findByUsername('users',user.email).then(dbUser => {
+       if(user.activation_token === dbUser.temporary_token){
         return bcrypt.hash(user.password, saltrounds)
         .then(hash => {
          return  queryHelper(
-           `UPDATE users SET password = ${hash},`+
-           `temporary_token = null, activated = true`+
+           `UPDATE users SET password = '${hash}',`+
+           `temporary_token = null, activated = true `+
             `WHERE email ='${dbUser.email}';
-           `)
+           `).then(user => {
+             return true
+           })
         })
        }else{
          Promise.reject();
