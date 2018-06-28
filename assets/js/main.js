4eac851f398e2fcf960c5974e9c822f76b7aecad
Filipe Costa <filipegraca@live.com.pt>
2018-06-28 15:58:57 +0100

    Modified css styles and javascript main file, added velocityjs animation library cdn to the homepage card-deck grid, it will now be working on IE9

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,28 @@
 // Grid
-const squareButtons = document.querySelectorAll(".square");
-// toggleSquares
-// Toggles all open squares
-const toggleAllSquares = function () {
-    const hide = "hide",
-        display = "display",
-        active = "active";
-    // For each square
-    // Toggle it
-    squareButtons.forEach(function (square) {
-        const squareTarget = square.querySelectorAll(".active > div");
-        // Reset each active square
-        squareTarget.forEach(function (content) {
-            if (content.classList.contains(display)) {
-                content.classList.remove(display);
-                content.classList.add(hide);
-                // Removes active class from each element
-                square.classList.remove(active);
-            } else {
-                content.classList.add(display);
-                content.classList.remove(hide);
-            }
-        });
-    });
+// Get all the cards nodes in the DOM
+const cards = document.querySelectorAll('.card-deck .card');
+// Function to display the current card
+const toggleCurrentCard = (card) => {
+    const cardBody = card.querySelector('.card-body');
+    const cardOverlay = card.querySelector('.card-overlay');
+    const cardTitle = card.querySelector('.card-title');
+    if(!card.classList.contains("active")){
+        card.classList.toggle("active");
+        cardTitle.velocity({display: "none"}, {duration: 0, queue: false});
+        cardOverlay.velocity({display: "block"}, {duration: 500, queue: false});
+        cardBody.velocity({ bottom: 85, display: "flex"}, {duration: 500, queue: false});
+    } else {
+        card.classList.toggle("active");
+        cardBody.velocity({ bottom: -85, display: "none"}, {duration: 500, queue: false});
+        cardOverlay.velocity({display: "none"}, {duration: 510, queue: false});
+        cardTitle.velocity({display: "block"}, {delay: 510, queue: false});  
+    }
 };
-// Show current square
-const showThisSquare = function (currentSquare) {
-    const squareTarget = currentSquare.querySelectorAll(".active > div");
-    const hide = "hide",
-        display = "display";
-    // For each square children element
-    // Check if it is hidden, then show it
-    // Otherwise, hide it
-    squareTarget.forEach(function (content) {
-        if (content.classList.contains(hide)) {
-            content.classList.remove(hide);
-            content.classList.add(display);
-        } else {
-            content.classList.remove(display);
-            content.classList.add(hide);
-        }
-    });
-};
-// Hides current square
-const hideThisSquare = function (currentTarget, currentTargetChildren) {
-    const active = "active",
-        display = "display",
-        hide = "hide";
-    const currentTargetTitle = currentTarget.querySelector(".square-title");
-    // For each square children element
-    // Check if it is hidden, then show it
-    // Otherwise, hide it
-    currentTargetChildren.forEach(function (content) {
-        if (content.classList.contains(display)) {
-            content.classList.remove(display);
-            content.classList.add(hide);
-        }
-        currentTargetTitle.classList.remove(hide);
-        currentTargetTitle.classList.add(display);
-    });
-    // Removes active class from the current square
-    currentTarget.classList.remove(active);
-};
-// Event Listeners
-// When any square is clicked
-// Display hidden elements of this square
-squareButtons.forEach(function (square) {
-    square.addEventListener("click", function (event) {
-        const currentTarget = event.currentTarget;
-        const currentTargetChildren = currentTarget.querySelectorAll(".active > div");
-        const active = "active",
-            display = "display",
-            hide = "hide";
-        if (currentTarget.classList.contains(active)) {
-            hideThisSquare(currentTarget, currentTargetChildren);
-        } else {
-            toggleAllSquares(); // resets open squares
-            currentTarget.classList.add(active);
-            showThisSquare(currentTarget);
-        }
+// For each card, add an event listener 'click'
+cards.forEach((cardBtn) => {
+    cardBtn.addEventListener('click', (event) => {
+        const card = event.currentTarget;
+        toggleCurrentCard(card); 
     }, false);
 });
 // Navbar
