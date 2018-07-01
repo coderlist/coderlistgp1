d5fb9a0f9b372ae19794655d018192eb237219fd
jidemobell <jidd32@gmail.com>
2018-07-01 23:42:28 +0100

    update nav items query

--- server/models/navigations.js
+++ server/models/navigations.js
@@ -3,48 +3,61 @@ const {queryHelper} = require('../../helperFunctions/query/queryHelper')
 
 module.exports = {
   
+ 
   /**
    * @param  {Object} bodyReq
-   * check table for required fields to update function
-   * accordingly
+   * parent_nav will be ordered by nav_order_number
    */
-  createNavItem(bodyReq){
+  createParentNavItem(bodyReq){
       return  queryHelper(`
         INSERT INTO navigations(name,title,link,nav_order_number,
-          grid_order_number,content) VALUES ('${bodyReq.name}','${bodyReq.title}',
-          '${bodyReq.link}',${bodyReq.navOrderNumber},${bodyReq.gridOrderNumber},
+          ,content) VALUES ('${bodyReq.name}','${bodyReq.title}',
+          '${bodyReq.link}',${bodyReq.navOrderNumber},
           '${bodyReq.content}')
        `).then(response => response)
         .catch(e =>{throw e})
   },
 
+  /**
+   * @param  {Object} bodyReq
+   * child_nav will be ordered by grid_order_number
+   */
   createChildNavItem(bodyReq){
     return queryHelper(`
-        INSERT INTO navigations(name,title,link,nav_order_number,
+        INSERT INTO navigations(name,title,link,
           grid_order_number,content,parent_navigation_id) VALUES ('${bodyReq.name}','${bodyReq.title}',
           '${bodyReq.link}',${bodyReq.navOrderNumber},${bodyReq.gridOrderNumber},
-          '${bodyReq.content}',${bodyReq.parentNavId})
+          '${bodyReq.content}',(SELECT navigation_id FROM navigations WHERE title=${bodyReq.parentTitle}))
        `).then(response => response)
         .catch(e =>{throw e})
   },
 
-  getNavItemByNavId(id){
+  getParentNavByNavId(id){
     return queryHelper(`
-      SELECT * FROM navigations WHERE navigation_id =${id}
+      SELECT * FROM navigations WHERE navigation_id =${id} AND 
+      parent_navigation_id IS NULL
     `).then(response => response)
     .catch(e =>{throw e})
   },
 
-  getNavItemByNavOrder(navOrder){
+  getParentNavByTitle(title){
     return queryHelper(`
-      SELECT * FROM navigations WHERE navigation_id =${navOrder}
+      SELECT * FROM navigations WHERE title =${title} AND 
+      parent_navigation_id IS NULL
+    `).then(response => response)
+    .catch(e =>{throw e})
+  },
+
+  getParentNavByNavOrder(navOrder){
+    return queryHelper(`
+      SELECT * FROM navigations WHERE nav_order_number =${navOrder}
     `).then(response => response)
     .catch(e =>{throw e})
   },
 
   getNavItemBygridOrder(gridOrder){
     return queryHelper(`
-    SELECT * FROM navigations WHERE navigation_id =${gridOrder}
+    SELECT * FROM navigations WHERE grid_order_number =${gridOrder}
   `).then(response => response)
    .catch(e =>{throw e})
   }
