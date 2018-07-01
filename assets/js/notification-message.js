e00e5d76f775dad00628ff0a7a16ba523ece9107
Filipe Costa <filipegraca@live.com.pt>
2018-07-01 18:18:25 +0100

    Delete user is submitting with a res status of 200 but no user is being deleted yet.

--- assets/js/notification-message.js
+++ assets/js/notification-message.js
@@ -3,12 +3,46 @@ const deleteThisButtons = document.querySelectorAll('.delete-this');
 const confirmDeleteButton = document.querySelector('.confirm-delete');
 const overlay = document.querySelector('.overlay');
 const title = document.querySelector('.overlay-alert-message h4').textContent;
-function deleteThis(url, id){
-    return fetch(url, {
-        method: 'POST',
+
+function deleteThisUser(url, user_id){
+    fetch(url, {
+        method: 'post',
         credentials: 'include',
-        body: id
-    }).then(response => console.log(response));
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: parseInt(user_id)
+        })
+    }).then(response => console.log(response.body))
+    .catch(error => console.log(`There was an error: ${error}`));
+}
+function deleteThisPage(url, page_id){
+    fetch(url, {
+        method: 'post',
+        credentials: 'include',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            page_id: parseInt(page_id)
+        })
+        
+    }).then(response => console.log(response.body))
+    .catch(error => console.log(`There was an error: ${error}`));
+}
+function deleteThisPDF(url, pdf_id){
+    fetch(url, {
+        method: 'post',
+        credentials: 'include',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: parseInt(pdf_id)
+        })
+    }).then(response => response)
+    .catch(error => console.log(`There was an error: ${error}`));
 }
 
 /* Toggle Warning Message */
@@ -16,28 +50,30 @@ const toggleAlertMessage = function () {
     overlay.classList.toggle("show");
 };
 const getDataFromInputFields = function (index){
-        console.log(index);
         const placeholder = document.querySelector('.strong');
+        const inputIdField = document.querySelector('.inputFieldId');
         let name = document.querySelectorAll('.this_name')[index].value;
         let id = document.querySelectorAll('.this_id')[index].value;
+        inputIdField.value = id;
         placeholder.textContent = name;
 };
-const confirmDeleteMessage = function (){
-    console.log(title);
+const confirmDeleteMessage = function (event){
+    event.preventDefault();
+    const inputIdField = document.querySelector('.inputFieldId');
+    let id = inputIdField.value;
+    console.log(id);
     switch(title){
-        case "Delete User": deleteThis('/delete-user',id);
+        case "Delete User": deleteThisUser('/users/delete-user', id);
             break;
-        case "Delete Page": deleteThis('/delete-page',id);
+        case "Delete Page": deleteThisPage('/users/delete-page', id);
             break;
-        case "Delete PDF": deleteThis('/delete-pdf',id);
+        case "Delete PDF": deleteThisPDF('/users/delete-pdf', id);
             break;
     }
-    event.preventDefault();
 }
 /* Attach Event Listeners to Buttons */
 confirmDeleteButton.addEventListener('click', function(event) {
-    confirmDeleteMessage();
-    
+    confirmDeleteMessage(event);
 });
 closeAlertMessageButtons.forEach(button => {
     button.addEventListener('click', function () {
