5f98c59be92608bb7a728d31a09e0bed5ffdbbfa
ksigston <ksigston@blueyonder.co.uk>
2018-07-03 21:02:51 +0100

    add .pdf to end  of displayed pdfs

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -214,7 +214,7 @@ userRoutes.get('/manage-pdfs', function (req, res) {
     }
       pdfs.map(function(pdf) {
       console.log('pdfs :', pdf);
-      const shortName = pdf.match(/([\w\s]*)/)[0];
+      const shortName = pdf.match(/([\w\s]*)/)[0] + ".pdf";
       console.log('shortName :', shortName + ".pdf");
 
       pdfList.push({name: pdf, short: shortName, location: `/pdfs/${pdf}`})
