6c7c167bde4189d1d87d7d8a312615fe57b1d23c
Filipe Costa <filipegraca@live.com.pt>
2018-07-11 07:50:47 +0100

    Nav items still not working as intended, there is a bug when creating a new nav item, the index is not the correct one.

--- assets/js/add-nav-item.js
+++ assets/js/add-nav-item.js
@@ -1,6 +1,6 @@
 /* 
-* Arrays to hold pages, parent pages and child pages names.
-*/
+ * Arrays to hold pages, parent pages and child pages names.
+ */
 const pageNames = ['no-link'];
 const parentPageName = [];
 const childPageName = [];
@@ -14,28 +14,28 @@ const init = {
     credentials: 'include'
 }
 fetch('/users/page-navmenu-request', init)
-        .then(function(response) {
-           return response.json();
-        }).then(function(data){
-            data.forEach(page => {
-                console.log(page.page);
-                /* If parent page has children */
-                if(page.children !== null){
-                    /* Get page name */
-                    parentPageName.push(page.page);
-                    /* Get children pages */
-                    page.children.forEach(child => {
-                        childPageName.push(child.page);
-                        console.log(child.page);
-                    });
-                    
-                }
-                /* Get Parent pages who don't have Children */
-                pageNames.push(page.page);
-            });
-        }).catch(function(error){
-            console.log("It wasn't possible to return any data from the server: ", error);
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        data.forEach(page => {
+            console.log(page.page);
+            /* If parent page has children */
+            if (page.children !== null) {
+                /* Get page name */
+                parentPageName.push(page.page);
+                /* Get children pages */
+                page.children.forEach(child => {
+                    childPageName.push(child.page);
+                    console.log(child.page);
+                });
+
+            }
+            /* Get Parent pages who don't have Children */
+            pageNames.push(page.page);
         });
+    }).catch(function (error) {
+        console.log("It wasn't possible to return any data from the server: ", error);
+    });
 /*
  *   Variables for
  *   Main menu navigation table
@@ -47,28 +47,28 @@ const tableMenuItem = document.querySelector(".table-menu-items");
 const addNewMenuItemButton = document.querySelectorAll(".add-new-item-button");
 const tableSubMenuItem = document.querySelector(".table-sub-menu-items");
 /* 
-* Important menu variables
-* parentMenuItemPage defines what kind of options this menu should render -> Parent Pages
-* childMenuItemPage defines what kind of options this menu should render -> Child of these Parent Pages
-* inputFieldName && inputFieldOrderNumber define what kind of input should be render
-* Input with for the Page Name or an input for an order number
-*/
+ * Important menu variables
+ * parentMenuItemPage defines what kind of options this menu should render -> Parent Pages
+ * childMenuItemPage defines what kind of options this menu should render -> Child of these Parent Pages
+ * inputFieldName && inputFieldOrderNumber define what kind of input should be render
+ * Input with for the Page Name or an input for an order number
+ */
 const parentMenuItemPage = "parentMenuItemPage";
 const childMenuItemPage = "childMenuItemPage";
 const inputFieldName = "inputFieldName";
 const inputFieldOrderNumber = "inputFieldOrderNumber";
 
 /* Create hidden input field to hold ids */
-const createMainNavItemHiddenInputIDField = function (){
+const createMainNavItemHiddenInputIDField = function () {
     return '<input type="hidden" class="main-nav-item-id" value="null"/>';
 };
-const createSubNavItemHiddenInputIDField = function (){
+const createSubNavItemHiddenInputIDField = function () {
     return '<input type="hidden" class="sub-nav-item-id" value="null"/>';
 };
 /* Creates Input Field */
 const createInputFieldPageName = function (inputFieldName) {
     let input = "";
-    switch(inputFieldName){
+    switch (inputFieldName) {
         case "inputFieldName":
             input = `<input type="text" class="form-control" name="page_name" placeholder="Page Name">`;
             break;
@@ -82,7 +82,7 @@ const createInputFieldPageName = function (inputFieldName) {
 const createSelectionMenu = function (pageName) {
     let options = '';
     let select = ``;
-    switch(pageName){
+    switch (pageName) {
         case "parentMenuItemPage":
             options = parentPageName.map((option, index) => {
                 return `<option value="${index}">${option}</option>`
@@ -120,16 +120,16 @@ const menuItemList = function () {
     /* Append all elements to the html variable */
     let html = '';
     html = createMainNavItemHiddenInputIDField() +
-        createInputFieldPageName(inputFieldName) + 
-        createSelectionMenu() + 
-        createInputFieldPageName(inputFieldOrderNumber) + 
+        createInputFieldPageName(inputFieldName) +
+        createSelectionMenu() +
+        createInputFieldPageName(inputFieldOrderNumber) +
         createButtonElement("save-menu-item");
     return html;
 };
 /* Submenu items have two extra elements */
 const subMenuItemList = function () {
     let html = '';
-    html = createSubNavItemHiddenInputIDField() + 
+    html = createSubNavItemHiddenInputIDField() +
         createInputFieldPageName(inputFieldName) +
         createSelectionMenu(parentMenuItemPage) +
         createSelectionMenu(childMenuItemPage) +
@@ -139,90 +139,127 @@ const subMenuItemList = function () {
 };
 /* Function to prepend the new item */
 const addNewMenuItem = function (event) {
-    if (event.target.classList.contains("sub-menu-item-button") ) {
+    if (event.target.classList.contains("sub-menu-item-button")) {
         /* Append the result of createTableData */
         const html = `<tr>${subMenuItemList()}</tr>`;
         /* Insert item before the first child */
         tableSubMenuItem.insertAdjacentHTML('afterbegin', html);
-        /* Remove Event Listeners */
-        removeEventListenersFromSubMenuItems();
     } else {
         /* Append the result of createTableData */
         const html = `<tr>${menuItemList()}</tr>`;
         /* Insert item before the first child */
         tableMenuItem.insertAdjacentHTML('afterbegin', html);
-        /* Remove Event Listeners */
-        removeEventListenersFromMenuItems();
     }
 };
 
 /* 
-*
-*   Add New Menu Item Buttons
-*
-*/
-addNewMenuItemButton.forEach( button => {
+ *
+ *   Add New Menu Item Buttons
+ *
+ */
+addNewMenuItemButton.forEach(button => {
     button.addEventListener('click', (event) => {
         addNewMenuItem(event);
     });
 });
 
 
-/* 
-*
-*   Get a live list of all the current save menu item buttons in the table and assign to variable saveNewMenuItemButton
-*   Get a live list of all the current save sub menu item buttons in the table and assign to variable saveNewMenuItemButton
-*
-*/
-
-let saveNewMenuItemButton = document.querySelectorAll(".save-menu-item");
-let saveNewSubMenuItemButton = document.querySelectorAll(".save-sub-menu-item");
 
 /* Save New Items into DB */
-function saveNewMenuItem(event, index){
-    console.log(index);
-};
-function saveNewSubMenuItem(event, index){
+function saveNewMenuItem(index) {
     console.log(index);
 };
 
-/* 
-*
-*   Remove event listeners from all the elements in the table
-*
-*/
-function removeEventListenersFromMenuItems(){
-    saveNewMenuItemButton.forEach( (button) => {
-        button.removeEventListener('click', saveNewMenuItem.bind(0), true);
-    });
+function saveNewSubMenuItem(index) {
+    console.log(index);
+};
+
+/**
+ * Get the tbody
+ */
+let menuTable = document.querySelector('.table-menu-items');
+let subMenuTable = document.querySelector('.table-sub-menu-items');
+
+/**
+ * 
+ * Gets the index of the respective row 
+ * Where the button is located at
+ * 
+ */
+
+
+function getMenuIndex(buttonTarget) {
+    /* Get updated state of the nodes */
+    menuTable = document.querySelector('.table-menu-items');
+    console.log(menuTable);
+    let buttonMenuIndex;
+    let rowIndex = buttonMenuIndex;
+    let found = false;
+    for (let i = 0, rows = menuTable.rows; i < rows.length; i++) {
+        const row = rows[i];
+        for (let j = 0, cells = row.cells; j < cells.length; j++) {
+            const cell = cells[j];
+            if (buttonTarget.classList.contains('save-menu-item')) {
+                console.log("Found it at index: ", i)
+                buttonMenuIndex = i;
+                found = !found;
+                break;
+            } else {
+                return;
+            }
+        }
+        if(found === true){
+            break;
+        }
+    }
+    found = !found;
+    console.log(buttonMenuIndex);
 }
-function removeEventListenersFromSubMenuItems(){
-    saveNewMenuItemButton.forEach( (button) => {
-        button.removeEventListener('click', saveNewMenuItem.bind(0), true);
-    });
+
+
+function getSubMenuIndex(buttonTarget) {
+    /* Get updated state of the nodes */
+    subMenuTable = document.querySelector('.table-sub-menu-items');
+    console.log(subMenuTable);
+    let buttonSubMenuIndex = 0;
+    let found = false;
+    for (let i = 0, rows = subMenuTable.rows; i < rows.length; i++) {
+        const row = rows[i];
+        for (let j = 0, cells = row.cells; j < cells.length; j++) {
+            const cell = cells[j];
+            if (buttonTarget.classList.contains('save-sub-menu-item')) {
+                console.log("Found it at index: ", i)
+                buttonSubMenuIndex = i;
+                found = !found;
+                break;
+            } else {
+                return;
+            }
+        }
+        if(found === true){
+            break;
+        }
+    }
+    found = false;
+    console.log(buttonSubMenuIndex);
 }
 
 /*
-*
-*
-*   Map new event listeners
-*
-*/
+ *
+ * Event delegation to help with dynamically created items:
+ * Not working yet, for some unknown reason
+ *
+ */
 
-/* 
-*
-*   Bind event listeners to the current live node save button elements.
-*
-*/
-
-saveNewMenuItemButton.forEach( (button, index) => {
-    button.addEventListener('click', (event) => {
-        saveNewMenuItem();
-    }, true);
+menuTable.addEventListener('click', event => {
+    const buttonTarget = event.target;
+    event.stopPropagation();
+    getMenuIndex(buttonTarget);
 });
 
-saveNewSubMenuItemButton.forEach( (button, index) => {
-    button.addEventListener('click', (event) => {
-        saveNewSubMenuItem();
-    }, true);
+subMenuTable.addEventListener('click', event => {
+    const buttonTarget = event.target;
+    event.stopPropagation();
+    getSubMenuIndex(buttonTarget);
 });
+
