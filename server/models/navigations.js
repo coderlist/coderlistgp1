420df65ca803a54157379a707f90257eb4eaf1dd
jidemobell <jidd32@gmail.com>
2018-07-14 18:24:15 +0100

    add get link queries to navigations

--- server/models/navigations.js
+++ server/models/navigations.js
@@ -51,11 +51,40 @@ module.exports = {
       `DELTE FROM page_navigations WHERE order_num=${num}`
     ).then(response => response)
     .catch(e =>{throw e})
-   }
+   },
+
+   
+   /**
+    * @param  {Int} id
+    * gets nav link information by page_id
+    */
+   getNavLinkByPageId(id){
+    return queryHelper(`
+   WITH temp_table AS(SELECT p.link,n.page_id FROM pages 
+    AS p FULL JOIN page_navigations AS n ON 
+    p.page_id = n.page_id;) SELECT link from 
+    temp_table where page_id=${id};
+    `).then(response => response)
+    .catch(e =>{throw e})
+   },
+
+   /**
+    * 
+    * gets all nav item information with link
+    */
+   getAllNavItemsWithLink(){
+     return queryHelper(`
+     SELECT p.link,n.item_id,n.title,n.page_id,n.parent_id,
+     n.order_num FROM pages as p FULL JOIN page_navigations 
+     AS n ON p.page_id = n.page_id;
+     `).then(response => response)
+     .catch(e =>{throw e})
+   },
+
+   
 
 
 
 
 } 
-
 
