2519ee57fd2f15cf1f1d688829ce2b1833a8d1fd
Filipe Costa <filipegraca@live.com.pt>
2018-07-15 23:11:43 +0100

    Added new files

---
+++ assets/js/manage-nav-message.js
@@ -0,0 +1,30 @@
+const manageNavOverlay = document.querySelector('.manage-nav-overlay');
+const closeManageNavMessageButton = document.querySelector('.close-manage-nav-message');
+let manageNavTimeOut;
+
+function toggleManageNavOverlay (){
+    if(manageNavOverlay !== null){
+        if(manageNavOverlay.classList.contains("hide")){
+            toggleManageNavMessage();
+        }
+    }
+}
+
+function toggleManageNavMessage(){
+    // toggle flashOverlay
+    // If it is displayed, hide it
+    // Otherwise, display it
+    manageNavOverlay.classList.toggle("hide");
+    // If the time out is not undefined
+    // It means it has a defined value
+    // clearTimeOut, will clear the setTimeOut method from the call stack 
+}
+
+function closeManageNavMessage(){
+    manageNavOverlay.classList.toggle("hide");
+}
+
+// Add event listeners
+closeManageNavMessageButton.addEventListener('click', function () {
+    closeManageNavMessage();
+});
