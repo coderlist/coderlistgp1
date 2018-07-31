7784a7f7ff27d5aaf0096c4b9a62f022b78b2581
Filipe Costa <filipegraca@live.com.pt>
2018-07-31 22:12:32 +0100

    Fixed issue with CKEditor not changing the variable to true due to not listening to dom events but to its own ones.

--- assets/js/alert-box.js
+++ assets/js/alert-box.js
@@ -1,12 +1,13 @@
-const inputFieldChanges = document.querySelectorAll('.this-field');
+const inputFieldListenForChanges = document.querySelectorAll('.this-field');
 let anyChanges = false;
+
 window.onbeforeunload = function(){
     if(anyChanges){
         return 'You may have unsaved changes, are you sure you want to leave the page?';
     }
 };
 
-inputFieldChanges.forEach((input) => {
+inputFieldListenForChanges.forEach((input) => {
     input.addEventListener('change', () => {
         anyChanges = true;
     });
