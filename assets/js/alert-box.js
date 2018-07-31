355cd54a1a9e1e1090a8cf7012fad61079a06cbc
Filipe Costa <filipegraca@live.com.pt>
2018-07-31 21:46:05 +0100

    Hotfix for any changes.

--- assets/js/alert-box.js
+++ assets/js/alert-box.js
@@ -1,5 +1,5 @@
 const inputFieldChanges = document.querySelectorAll('.this-field');
-
+let anyChanges = false;
 window.onbeforeunload = function(){
     if(anyChanges){
         return 'You may have unsaved changes, are you sure you want to leave the page?';
