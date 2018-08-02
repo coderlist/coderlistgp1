ab38d16bfac55883d5845dfcf8e87f418023f4b9
Filipe Costa <filipegraca1992@gmail.com>
2018-08-02 11:32:26 +0100

    Fixed an issue with browser alert messages when submitting/updating/deleting a page. Fixed the styling issues for the grid as well.

--- assets/js/alert-box.js
+++ assets/js/alert-box.js
@@ -1,4 +1,5 @@
 const inputFieldListenForChanges = document.querySelectorAll('.this-field');
+const resetBrowserActionButtons = document.querySelectorAll('.reset-browser-action');
 let anyChanges = false;
 
 window.onbeforeunload = function(){
@@ -6,6 +7,16 @@ window.onbeforeunload = function(){
         return 'You may have unsaved changes, are you sure you want to leave the page?';
     }
 };
+
+const unloadBrowserAction = () => {
+    window.onbeforeunload = null;
+} 
+
+resetBrowserActionButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        unloadBrowserAction();
+    });
+});
 
 inputFieldListenForChanges.forEach((input) => {
     input.addEventListener('change', () => {
